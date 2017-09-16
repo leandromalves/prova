0068ed93b8c1449366c6039064a3eb1b2e077913
lmouralves <lmouralves@gmail.com>
2017-09-16 14:16:03 -0300

    primeiro commit

---
+++ src/main/java/br/com/ecad/prova/App.java
@@ -0,0 +1,13 @@
+package br.com.ecad.prova;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
